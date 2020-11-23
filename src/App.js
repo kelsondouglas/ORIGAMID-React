62f7311d7cff0ff5963731019d064e5f6a46d1b1
Kelson Douglas <kelsondouglas23@hotmail.com>
2020-11-23 10:44:05 -0300

    Aula 2 - estrutura inicial

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import './App.css';
 import React from 'react'
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import Home from './Components/Home'
+import Login from './Components/Login/login'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
       <Footer />
+      </BrowserRouter>
     </div>
   );
 }
