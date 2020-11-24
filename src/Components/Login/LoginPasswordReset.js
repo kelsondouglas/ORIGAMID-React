df20799a92448236968e9785d0676174da7c74d5
Kelson Douglas <kelsondouglas23@hotmail.com>
2020-11-24 09:09:47 -0300

    Login Routes

--- src/Components/Login/LoginPasswordReset.js
+++ src/Components/Login/LoginPasswordReset.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Home = () => {
+const LoginPasswordReset = () => {
     return (
         <div>
             
@@ -8,4 +8,4 @@ const Home = () => {
     )
 }
 
-export default Home
+export default LoginPasswordReset
