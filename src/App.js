f745d1e02c4bda359272fb1a5c3a092898b633d2
Kelson Douglas <kelsondouglas23@hotmail.com>
2021-12-17 09:36:59 -0300

    18 UserHeader

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Home from "./Components/Home";
 import Login from "./Components/Login/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { UserStorage } from "./UserContext";
+import ProtectedRoute from "./Components/Helper/ProtectedRoute";
+import User from "./Components/User/User";
 
 function App() {
   return (
@@ -15,7 +17,15 @@ function App() {
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login/*" element={<Login />} />
+            <Route path="login/*" element={<Login />} />
+            <Route
+              path="conta/*"
+              element={
+                <ProtectedRoute>
+                  <User />
+                </ProtectedRoute>
+              }
+            />
           </Routes>
           <Footer />
         </UserStorage>
