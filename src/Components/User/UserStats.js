fe842cd2d7b4cf86b577282dcf405080f5f0ea41
Kelson Douglas <kelsondouglas23@hotmail.com>
2022-01-04 10:32:18 -0300

    38 graphs 01

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,12 +1,34 @@
 import React from "react";
+import { STATS_GET } from "../../api";
+import useFetch from "../../Hooks/useFetch";
+import Error from "../Helper/Error";
 import Head from "../Helper/Head";
+import Loading from "../Helper/Loading";
+import UserStatsGraphs from "./UserStatsGraphs";
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title="Estatísticas" />
-    </div>
-  );
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+
+      await request(url, options);
+    }
+
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data)
+    return (
+      <div>
+        <Head title="Estatísticas" />
+        <UserStatsGraphs data={data} />
+      </div>
+    );
+  else return null;
 };
 
 export default UserStats;
